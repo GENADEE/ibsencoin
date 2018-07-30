6e89f9440a0907ca070e6a7f0df0da4c355e1e5c
spagovir <edgarlin2000@gmail.com>
2018-07-30 15:25:03 -0700

    Switched hash for pow to sha-512; tested pow.

--- pow_tester.py
+++ pow_tester.py
@@ -1,6 +1,6 @@
 import proof_of_work
-for i in range(1,100):
+for i in range(1,10):
     print("...")
     rand_str = str(i)
-    proof_of_work.pow(rand_str,2)
+    proof_of_work.pow(rand_str,8)
     pass
