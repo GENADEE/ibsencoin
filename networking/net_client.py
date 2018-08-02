9f5a0daff2b9756ea7c4ae82f8c5af4dd140b33e
18adrianoh <adrianohernandez2000gmail.com>
2018-08-01 17:04:27 -0700

    ...

--- networking/net_client.py
+++ networking/net_client.py
@@ -2,6 +2,8 @@ import httplib
 import urllib
 import requests
 
+from .. import client # fix this import please
+
 #https://www.geeksforgeeks.org/get-post-requests-using-python/
 # ^^^ tutoial for requests
 class NClient:
@@ -10,7 +12,12 @@ class NClient:
         self.data = {'url'='http://127.0.0.1:5000/','params':{'message'='this is a test message'}}
         pass
 
-    #data should be {'url':URL,'params':PARAMS_JSON}
+    def request_transaction(self,data=self.data):
+        transaction = client.create_transaction(data['inputs'],data['n_outputs_groups'],data['v_output_groups'],data['output_group'],data['timeout'])
+        self.post_request(transaction) # fix this so it uses blinds properly
+        pass
+    #these are to make work easier later
+    #data should be {'url':URL,'params':PARAMS_AS_A_JSON}
     def get_request(self,data=self.data):
         return requests.get(url = data['url'], params = data['params'])
     def post_request(self,data=self.data):
--- networking/net_server.py
+++ networking/net_server.py
@@ -5,45 +5,43 @@
 #
 # (whatabout http://ecomunsing.com/build-your-own-blockchain)
 # flask run --host=0.0.0.0 to make it readable from other computers on the network
-import hashlib
+from .. import node # fix this import please
 import json
-from textwrap import dedent
-from time import time
 from uuid import uuid4
 
 from flask import Flask
-class NServer:
-    # has instance of coin server/node object
-    def __init__(self):
-        '''
-        # Instantiate our Node
-        self.app = Flask(__name__)
 
-        # Generate a globally unique address for this node/server
-        self.identifier = str(uuid4()).replace('-', '')
-
-        @app.route('/test', methods=['GET']) #test response
-        '''
-        pass
-    pass
-
-print 'start'
+identifier = str(uuid4()).replace('-', '')
 
 app = Flask(__name__)
 
+# this is an example app route
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# requests for pool and transaction will provide conditions for the requests to be met
+
+# params for pool should be {'size':<int:size>,'amount':<int:amount>}
 @app.route('/request_pool', methods=['POST'])
 def request_pool():
-     values = request.get_json()
+    values = request.get_json()
     return 'please wait'
+
+# once a pool has been accepted, the node will be able to verify transaction presented by a client
+# transaction should be a json with inputs, n_output_groups, v_output_groups, output_group, and timeout parameters
+# where (as in the client python file):
+#inputs: a list of unspent outputs. The format of an unspent output is a tuple of the format (transaction, pkey), where transaction is the hash of a complete signed, confirmed, transaction in the blockchain, and pkey is the public key associated with the ouput of that transaction to be spent.
+#n_output_groups: specifies the number of output groups; ie. the number of transactions that share the same input as this one, including this one. 
+#v_output_groups: specifies the value of each output group.
+#output_group: lists the outputs to this part of this transaction; the form is a list of tuples of the format (value, key).
+#timeout: unix time after which the transaction becomes invalid if it has not been confirmed. 
 @app.route('/verify_transaction', methods=['POST'])
 def verify_transaction():
-     values = request.get_json()
+    values = request.get_json()
+    node.verify_transaction_part(values) # fix this
     return 'verifying'
 
-print 'end'
 
 
 # ok, so the easiest way to do this will be to run a flask server for our nodes and then have the clients send requests
